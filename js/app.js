68bde00ae0657a0f167ba32a2a8822d026267fdf
Eric Snell <eric_snell21@yahoo.com>
2017-05-13 11:32:47 -0400

    es6 syntax -- game doesn't fire when space between tiles are clicked

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-(function() {
-  
-  var game = {
+(() => {
+
+  /*___Variables__*/
+
+  const game = {
     board: [
       [null, null, null],
       [null, null, null],
@@ -8,123 +10,113 @@
     ]
   }
   
-  var gameBoard = game.board.map(function(arr) {
-    return arr.slice();
-  }),
-      currentPlayer = 1,
+  let gameBoard = game.board.map(arr => arr.slice())
+
+  let currentPlayer = 1,
       $board = document.getElementById('board'),
       $results = document.getElementById('results'),
       $resultsText = $results.querySelector('#results-text'),
-      $newGameButton = $results.querySelector('#new-game');
+      $newGameButton = $results.querySelector('#new-game')
   
-         
-  $board.addEventListener('click', play);
-  
+
+  /*___Main Game Functionality___*/
+
+  $board.addEventListener('click', play)
    
   function play(event) {
-    var $tile = event.target,
-        row = $tile.dataset.row,
-        column = $tile.dataset.column;
-    
-    if (!gameBoard[row][column]) {
-      colorTile($tile);
-      updateBoard(row, column);
-      findWinner(row, column);
-    } else {
-      console.log('TRY AGAIN!');
-    }
+    if (event.target.tagName === 'DIV') {
+      let $tile = event.target,
+          row = $tile.dataset.row,
+          column = $tile.dataset.column;
+      
+      if (!gameBoard[row][column]) {
+        colorTile($tile)
+        updateBoard(row, column)
+        findWinner(row, column)
+      } else {
+        console.log('TRY AGAIN!')
+      }
+    } 
   } 
    
   function findWinner(row, column) {
     if (checkDiagonalTop() || checkDiagonalBottom() || checkRows(row) || checkColumns(column)) {
-      showResults(`Player ${currentPlayer} Wins!`);
+      showResults(`Player ${currentPlayer} Wins!`)
     } else if (isDraw()) {
-      showResults('Draw');
+      showResults('Draw')
     } else {
-      changePlayer();
+      changePlayer()
     }
-  } 
+  }
+
+
+  /*___Helper Functions__*/
    
   function colorTile(tile) {
     currentPlayer === 1 ? 
-      tile.classList.add('playerOne') : tile.classList.add('playerTwo');
+      tile.classList.add('playerOne') : tile.classList.add('playerTwo')
   }
   
   function updateBoard(row, column) {
-    gameBoard[row][column] = currentPlayer;
+    gameBoard[row][column] = currentPlayer
   }
   
   function changePlayer() {
-    currentPlayer = (currentPlayer === 1) ? 2 : 1;
+    currentPlayer = (currentPlayer === 1) ? 2 : 1
   }
   
   function checkRows(row) {
     for (var i = 0; i < gameBoard[row].length; i++) {
-      if (gameBoard[row][i] !== currentPlayer) {
-        return;
-      }
+      if (gameBoard[row][i] !== currentPlayer) return
     } 
-    return true;
+    return true
   }
   
   function checkColumns(column) {
     for (var i = 0; i < gameBoard.length; i++) {
-      if (gameBoard[i][column] !== currentPlayer) {
-        return;
-      }
+      if (gameBoard[i][column] !== currentPlayer) return
     }
     return true
   }
   
   function checkDiagonalTop() { 
     for (var i = 0; i < gameBoard.length; i++) {
-      if (gameBoard[i][i] !== currentPlayer) {
-        return;
-      } 
+      if (gameBoard[i][i] !== currentPlayer) return
     }
-    return true;
+    return true
   }
   
   function checkDiagonalBottom() {
     for (var i = 0; i < gameBoard.length; i++) {
-      if (gameBoard[i][(gameBoard.length - 1) - i] !== currentPlayer) {
-        return;
-      }
+      if (gameBoard[i][(gameBoard.length - 1) - i] !== currentPlayer) return
     }
-    return true;
+    return true
   }
 
   function isDraw() {
     for (var i = 0; i < gameBoard.length; i++) {
-      if (gameBoard[i].indexOf(null) !== -1) {
-        return;
-      }
+      if (gameBoard[i].indexOf(null) !== -1) return
     }
-    return true;
+    return true
   }
   
   function showResults(resultText) {
-    $board.removeEventListener('click', play);
-    $newGameButton.addEventListener('click', newGame);
-    $results.classList.remove('hidden');
-    $resultsText.innerHTML = resultText;
+    $board.removeEventListener('click', play)
+    $newGameButton.addEventListener('click', newGame)
+    $results.classList.remove('hidden')
+    $resultsText.innerHTML = resultText
   }
   
   function newGame() {
-    $results.classList.add('hidden');
+    var $tiles = $board.querySelectorAll('.playerOne, .playerTwo')
+    gameBoard = game.board.map(arr => arr.slice())
 
-    var $tiles = $board.querySelectorAll('.playerOne, .playerTwo');
-    $tiles.forEach(function(elm) {
-      elm.classList.remove('playerOne', 'playerTwo');
-    })
-        
-    gameBoard = game.board.map(function(arr) {
-      return arr.slice();
-    });
-    
-    $newGameButton.removeEventListener('click', newGame);
-    $board.addEventListener('click', play);
+    $tiles.forEach(elm => elm.classList.remove('playerOne', 'playerTwo'))
+
+    $results.classList.add('hidden')
+
+    $newGameButton.removeEventListener('click', newGame)
+    $board.addEventListener('click', play)
   }
      
-  
-})(); 
+})()
