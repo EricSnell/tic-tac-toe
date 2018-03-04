60c43633a97058115ad61e6eab8ecb346d36afcb
eric <ericmichaelsnell@gmail.com>
2018-03-04 17:37:13 -0500

    npm packages -- eslint -- scss -- fixed eslint errors in js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 (() => {
-
-  /*___Variables__*/
+  'use strict';
 
   const game = {
     board: [
@@ -9,114 +8,115 @@
       [null, null, null]
     ]
   }
-  
+
   let gameBoard = game.board.map(arr => arr.slice())
+  let currentPlayer = 1;
 
-  let currentPlayer = 1,
-      $board = document.getElementById('board'),
-      $results = document.getElementById('results'),
-      $resultsText = $results.querySelector('#results-text'),
-      $newGameButton = $results.querySelector('#new-game')
-  
-
-  /*___Main Game Functionality___*/
+  const $board = document.getElementById('board');
+  const $results = document.getElementById('results');
+  const $resultsText = $results.querySelector('#results-text');
+  const $newGameButton = $results.querySelector('#new-game');
 
   $board.addEventListener('click', play)
-   
+
+
+
+
   function play(event) {
     if (event.target.tagName === 'DIV') {
-      let $tile = event.target,
-          row = $tile.dataset.row,
-          column = $tile.dataset.column;
-      
+      const $tile = event.target;
+      const { row } = $tile.dataset;
+      const { column } = $tile.dataset;
+
       if (!gameBoard[row][column]) {
-        colorTile($tile)
-        updateBoard(row, column)
-        findWinner(row, column)
+        colorTile($tile);
+        updateBoard(row, column);
+        findWinner(row, column);
       } else {
-        console.log('TRY AGAIN!')
+        console.log('TRY AGAIN!');
       }
-    } 
-  } 
-   
+    }
+  }
+
   function findWinner(row, column) {
     if (checkDiagonalTop() || checkDiagonalBottom() || checkRows(row) || checkColumns(column)) {
-      showResults(`Player ${currentPlayer} Wins!`)
+      showResults(`Player ${currentPlayer} Wins!`);
     } else if (isDraw()) {
-      showResults('Draw')
+      showResults('Draw');
     } else {
-      changePlayer()
+      changePlayer();
     }
   }
 
-
-  /*___Helper Functions__*/
-   
   function colorTile(tile) {
-    currentPlayer === 1 ? 
-      tile.classList.add('playerOne') : tile.classList.add('playerTwo')
+    if (currentPlayer === 1) {
+      tile.classList.add('playerOne');
+    }
+    else {
+      tile.classList.add('playerTwo');
+    }
   }
-  
+
   function updateBoard(row, column) {
-    gameBoard[row][column] = currentPlayer
+    gameBoard[row][column] = currentPlayer;
   }
-  
+
   function changePlayer() {
-    currentPlayer = (currentPlayer === 1) ? 2 : 1
+    currentPlayer = (currentPlayer === 1) ? 2 : 1;
   }
-  
+
   function checkRows(row) {
-    for (var i = 0; i < gameBoard[row].length; i++) {
-      if (gameBoard[row][i] !== currentPlayer) return
-    } 
-    return true
+    for (let i = 0; i < gameBoard[row].length; i++) {
+      if (gameBoard[row][i] !== currentPlayer) return;
+    }
+    return true;
   }
-  
+
   function checkColumns(column) {
-    for (var i = 0; i < gameBoard.length; i++) {
-      if (gameBoard[i][column] !== currentPlayer) return
+    for (let i = 0; i < gameBoard.length; i++) {
+      if (gameBoard[i][column] !== currentPlayer) return;
     }
-    return true
+    return true;
   }
-  
-  function checkDiagonalTop() { 
-    for (var i = 0; i < gameBoard.length; i++) {
-      if (gameBoard[i][i] !== currentPlayer) return
+
+  function checkDiagonalTop() {
+    for (let i = 0; i < gameBoard.length; i++) {
+      if (gameBoard[i][i] !== currentPlayer) return;
     }
-    return true
+    return true;
   }
-  
+
   function checkDiagonalBottom() {
-    for (var i = 0; i < gameBoard.length; i++) {
-      if (gameBoard[i][(gameBoard.length - 1) - i] !== currentPlayer) return
+    for (let i = 0; i < gameBoard.length; i++) {
+      if (gameBoard[i][(gameBoard.length - 1) - i] !== currentPlayer) return;
     }
-    return true
+    return true;
   }
 
   function isDraw() {
-    for (var i = 0; i < gameBoard.length; i++) {
-      if (gameBoard[i].indexOf(null) !== -1) return
+    for (let i = 0; i < gameBoard.length; i++) {
+      if (gameBoard[i].indexOf(null) !== -1) return;
     }
-    return true
+    return true;
   }
-  
+
   function showResults(resultText) {
-    $board.removeEventListener('click', play)
-    $newGameButton.addEventListener('click', newGame)
-    $results.classList.remove('hidden')
-    $resultsText.innerHTML = resultText
+    $board.removeEventListener('click', play);
+    $newGameButton.addEventListener('click', newGame);
+    $results.classList.remove('hidden');
+    $resultsText.innerHTML = resultText;
   }
-  
+
   function newGame() {
-    var $tiles = $board.querySelectorAll('.playerOne, .playerTwo')
-    gameBoard = game.board.map(arr => arr.slice())
+    const $tiles = $board.querySelectorAll('.playerOne, .playerTwo');
+    gameBoard = game.board.map(arr => arr.slice());
 
-    $tiles.forEach(elm => elm.classList.remove('playerOne', 'playerTwo'))
+    $tiles.forEach(elm => elm.classList.remove('playerOne', 'playerTwo'));
 
-    $results.classList.add('hidden')
+    $results.classList.add('hidden');
 
-    $newGameButton.removeEventListener('click', newGame)
-    $board.addEventListener('click', play)
+    $newGameButton.removeEventListener('click', newGame);
+    $board.addEventListener('click', play);
   }
-     
+
 })()
