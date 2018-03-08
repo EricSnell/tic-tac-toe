85fcac76ca340a03ad9d00448e90a38fabc5c466
eric <ericmichaelsnell@gmail.com>
2018-03-07 20:29:43 -0500

    updated diagonal block/win logic for computer -- now blocks/attempts to win all cases

--- js/app.js
+++ js/app.js
@@ -52,6 +52,8 @@
       else if (gameBoard[2][2] === computer && gameBoard[1][1] === computer && gameBoard[0][0] === null) { computerPlay(0, 0); }
       else if (gameBoard[2][0] === computer && gameBoard[1][1] === computer && gameBoard[0][2] === null) { computerPlay(0, 2); }
       else if (gameBoard[0][2] === computer && gameBoard[1][1] === computer && gameBoard[2][0] === null) { computerPlay(2, 0); }
+      else if (gameBoard[0][0] === computer && gameBoard[2][2] === computer && gameBoard[1][1] === null) { computerPlay(1, 1); }
+      else if (gameBoard[2][0] === computer && gameBoard[0][2] === computer && gameBoard[1][1] === null) { computerPlay(1, 1); }
       else { blockPlayer(); }
     }
 
@@ -77,6 +79,8 @@
       else if (gameBoard[2][2] === player1 && gameBoard[1][1] === player1 && gameBoard[0][0] === null) { computerPlay(0, 0); }
       else if (gameBoard[2][0] === player1 && gameBoard[1][1] === player1 && gameBoard[0][2] === null) { computerPlay(0, 2); }
       else if (gameBoard[0][2] === player1 && gameBoard[1][1] === player1 && gameBoard[2][0] === null) { computerPlay(2, 0); }
+      else if (gameBoard[0][0] === player1 && gameBoard[2][2] === player1 && gameBoard[1][1] === null) { computerPlay(1, 1); }
+      else if (gameBoard[2][0] === player1 && gameBoard[0][2] === player1 && gameBoard[1][1] === null) { computerPlay(1, 1); }
       else { random(); }
     }
 
